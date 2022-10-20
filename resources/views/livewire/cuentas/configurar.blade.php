8f2db7383f4c049a85dd9432a616c14fdc6a68e5
dag-98 <daguilar.pacheco98@gmail.com>
2022-10-19 18:57:41 -0600

    Vistas de configurar cuentas y registro periodo contable

--- resources/views/livewire/cuentas/configurar.blade.php
+++ resources/views/livewire/cuentas/configurar.blade.php
@@ -1,3 +1,244 @@
 <div>
-    {{-- Close your eyes. Count to one. That is how long forever feels. --}}
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
+        
+    </head>
+
+    <body>
+        <br>
+        <div class="container">
+            <div class="row g-3">
+                <h1>Configuración de cuentas para cálculo de Ratios</h1>
+            </div>
+        </div>
+        <br>
+        <div class="container">
+                
+                <h2>Cuentas Mayores</h1>
+                <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Activo</label>
+                </div>
+                <div class="col-6 col-md-4 ">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Cuenta Mayor</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Pasivo</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Cuenta Mayor</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Capital</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Cuenta Mayor</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+                
+        </div>
+        <br>
+        <div class="container">
+                <h2>Cuentas</h1>
+                <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Activos Corriente</label>
+                </div>
+                <div class="col-6 col-md-4 ">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Cuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Pasivos Corriente</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Cuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Utilidad Neta</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Cuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Utilidad Operativa</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Cuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Utilidad Bruta</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Cuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Activo Fijo Neto</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Cuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+                
+        </div>
+        <div class="container">
+                <h2>Subcuentas</h1>
+                <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Gastos Financieros</label>
+                </div>
+                <div class="col-6 col-md-4 ">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Subcuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Inventario</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Subcuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Cuentas por Pagar</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Subcuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Costo de Venta</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Subcuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row justify-content-md-center">
+                <div class="col-6 col-md-4">
+                    <label for="exampleFormControlInput1" class="form-label">Cuentas por Cobrar</label>
+                </div>
+                <div class="col-6 col-md-4">
+                    <select class="form-select" aria-label="Default select example">
+                        <option selected>Subcuenta</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+            <br>
+            <div class="row">
+                <div class="col-auto">
+                     <button type="submit" class="btn btn-primary mb-3">Aceptar</button>
+                </div>
+            </div>
+            <br>
+            
+    
+                
+        </div>
+           
+        
+    </body>
 </div>
