1b5ac073e8b473e94594d84fc8d97956663e2a4f
Andrea Monterrosa <moralesandrea.edr4@gmail.com>
2022-11-02 01:19:58 +0000

    guardar archivos de excel

--- app/Http/Livewire/Estados/CargarEstados.php
+++ app/Http/Livewire/Estados/CargarEstados.php
@@ -3,9 +3,22 @@
 namespace App\Http\Livewire\Estados;
 
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class CargarEstados extends Component
 {
+    use WithFileUploads;
+ 
+    public $estadosFinancieros;
+ 
+    public function save()
+    {
+        $this->validate([
+            'estadosFinancieros' => 'file|max:1024|mimes:xlsx', // 1MB Max
+        ]);
+ 
+        $this->estadosFinancieros->store('public');
+    }
     public function render()
     {
         return view('livewire.estados.cargar-estados');
