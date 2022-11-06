0942d0d7dd62ad7d7bca9889edf904d8b04e4edb
Andrea Monterrossa <moralesandrea.edr4@gmail.com>
2022-11-06 02:33:12 +0000

    refactor

--- app/Imports/EstadosFinancierosImport.php
+++ app/Imports/EstadosFinancierosImport.php
@@ -1,33 +1,5 @@
 <?php
 
-// namespace App\Imports;
-
-// use Maatwebsite\Excel\Row;
-// use Maatwebsite\Excel\Concerns\OnEachRow;
-// use Ramsey\Uuid\Type\Integer;
-
-// class EstadosFinancierosImport implements OnEachRow
-// {
-//     private static Integer $cuentaMayorContador;
-//     private static Integer $cuentaContador;
-//     private static Integer $subCuentaContador;
-
-//     public function onRow(Row $row)
-//     {
-//         $rowIndex = $row->getIndex();
-//         $row      = $row->toArray();
-        
-//         // $group = Group::firstOrCreate([
-//         //     'name' => $row[1],
-//         // ]);
-    
-//         // $group->users()->create([
-//         //     'name' => $row[0],
-//         // ]);
-//     }
- 
-// }
-
 namespace App\Imports;
 
 use Illuminate\Support\Collection;
@@ -45,13 +17,6 @@ class EstadosFinancierosImport implements ToCollection
                             'valor'=>$row[1]
                         ]);
         }
-        // foreach ($rows as $row) 
-        // {
-        //     $cuenta[]=([
-        //         'name' => $row[0],
-        //     ]);
-        // }
-        // return $cuenta;
         dd($cuenta);
     }
 }
